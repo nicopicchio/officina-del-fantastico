a230c1fa493a4746252378035fb712611a437052
Nico Picchio <nicolapicchio@gmail.com>
2022-04-04 13:19:24 +0100

    logo and navigation added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
 import { Routes, Route } from 'react-router-dom';
+import mainLogo from '../src/images/main-logo.png';
 
 function App() {
 	return (
-		<div className="main-page-container">
+		<div className='main-page-container'>
 			<header>
-
-      </header>
+				<div className='header-left'>
+          <img src={mainLogo} alt="main logo" width="300px" />
+        </div>
+				<div className='header-right'>
+					<nav>
+						<ul className='navigation'>
+							<li>Progetto</li>
+							<li>Tours</li>
+							<li>About</li>
+							<li>Partner</li>
+							<li>Contact</li>
+						</ul>
+					</nav>
+				</div>
+			</header>
 			<main></main>
 			<footer></footer>
 		</div>
