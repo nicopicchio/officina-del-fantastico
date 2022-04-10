d63b9274216e02acdb2b543193da00b31395603a
Nico Picchio <nicolapicchio@gmail.com>
2022-04-10 15:09:35 +0100

    banner changed, new photo added, implemented photo layout in home

--- src/App.js
+++ src/App.js
@@ -1,6 +1,4 @@
 import { Routes, Route } from 'react-router-dom';
-import img1 from '../src/images/image-1.jpg';
-import img2 from '../src/images/image-2.jpg';
 import Header from './components/Header/Header'
 import Main from './components/Main/Main';
 import Footer from './components/Footer/Footer'
--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,5 +1,8 @@
 import img1 from '../../../src/images/image-1.jpg';
 import img2 from '../../../src/images/image-2.jpg';
+import img3 from '../../../src/images/image-3.jpg';
+import img4 from '../../../src/images/image-4.jpg';
+import img5 from '../../../src/images/image-5.jpg';
 
 function Main() {
 	return (
@@ -7,6 +10,23 @@ function Main() {
 			<section className='main-section-links'>
 				<h2>CALENDARIO</h2>
 				<h2>PIANIFICA TOUR</h2>
+			</section>
+			<section className='main-img-container'>
+				<div className='main-single-img-container'>
+					<img src={img1} alt='' width='100%'/>
+				</div>
+				<div className='main-single-img-container'>
+					<img src={img2} alt='' width='100%'/>
+				</div>
+				<div className='main-single-img-container'>
+					<img src={img5} alt='' width='100%'/>
+				</div>
+				<div className='main-single-img-container'>
+					<img src={img4} alt='' width='100%'/>
+				</div>
+				<div className='main-single-img-container'>
+					<img src={img3} alt='' width='100%'/>
+				</div>
 			</section>
 		</main>
 	);
